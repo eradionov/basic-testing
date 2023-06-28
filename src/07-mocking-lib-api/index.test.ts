3566fc70561e5399bc4749a6762b7c56ce8c9477
eugene.radionov <eugene.radionov@jagaad.it>
2023-06-28 22:18:05 +0200

    implement: complete tasks 7-8

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,63 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest
+      .fn()
+      .mockResolvedValueOnce({ data: { message: 'Got Busted' } });
+    const mockCreate = jest.spyOn(axios, 'create').mockReturnValueOnce({
+      get: mockGet,
+    } as unknown as AxiosInstance);
+
+    await throttledGetDataFromApi('/test');
+
+    jest.runAllTimers();
+
+    expect(mockCreate).toHaveBeenCalledTimes(1);
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest
+      .fn()
+      .mockResolvedValueOnce({ data: { message: 'Got Busted' } });
+    jest.spyOn(axios, 'create').mockReturnValueOnce({
+      get: mockGet,
+    } as unknown as AxiosInstance);
+
+    await throttledGetDataFromApi('/test');
+    jest.runAllTimers();
+    expect(mockGet).toHaveBeenCalledTimes(1);
+    expect(mockGet).toHaveBeenCalledWith('/test');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockedResponse = { message: 'Got Busted' };
+
+    const mockGet = jest
+      .fn()
+      .mockResolvedValueOnce({ data: mockedResponse });
+    jest.spyOn(axios, 'create').mockReturnValueOnce({
+      get: mockGet,
+    } as unknown as AxiosInstance);
+
+    const result = await throttledGetDataFromApi('/test');
+    jest.runAllTimers();
+
+    expect(result).toEqual(mockedResponse);
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1];
+
+    const linkedList = generateLinkedList(elements);
+
+    expect(linkedList).toStrictEqual({
+      next: { next: null, value: null },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [1, 2];
+
+    const linkedList = generateLinkedList(elements);
+
+    expect(linkedList).toStrictEqual({
+      next: { next: { next: null, value: null }, value: 2 },
+      value: 1,
+    });
   });
 });
